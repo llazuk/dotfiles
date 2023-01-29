da5dc3b38e7059cec5c1d7b4b5a13fdb5f2ef0ac
llazuk <mailujspam@gmail.com>
2023-01-29 15:30:54 +1100

    2023-01-29

--- lua/kss/ide/treesitter.lua
+++ lua/kss/ide/treesitter.lua
@@ -2,7 +2,7 @@ require'nvim-treesitter.configs'.setup {
     ensure_installed = { "c", "lua", "javascript", "json", "python", "latex"},
     highlight = {
         enable = true,
-        additional_vim_regex_highlighting = false,
+        additional_vim_regex_highlighting = {"latex"}
     },
     indent = {
         enable = true
