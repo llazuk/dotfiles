6caa026ec07f08666a365022877b429bc3c8ef7c
llazuk <mailujspam@gmail.com>
2023-02-04 23:43:39 +1100

    2023-02-04

--- lua/kss/ide/cmp.lua
+++ lua/kss/ide/cmp.lua
@@ -59,25 +59,25 @@ cmp.setup({
     }),
     -- lspkind plugin section
     formatting = {
-    format = lspkind.cmp_format({
-      mode = 'text_symbol', -- show only symbol annotations
-      maxwidth = 80, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-    menu = ({
-      buffer = "[Buffer]",
-      nvim_lsp = "[LSP]",
-      luasnip = "[LuaSnip]",
-      nvim_lua = "[Lua]",
-      latex_symbols = "[Latex]",
-    })
-      -- The function below will be called before any actual modifications from lspkind
-      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-      -- before = function (entry, vim_item)
-      --   ...
-      --   return vim_item
-      -- end
-    })
-  }
+        format = lspkind.cmp_format({
+            mode = 'text_symbol', -- show only symbol annotations
+            maxwidth = 80, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+            menu = ({
+                buffer = "[Buffer]",
+                nvim_lsp = "[LSP]",
+                luasnip = "[LuaSnip]",
+                nvim_lua = "[Lua]",
+                latex_symbols = "[Latex]",
+            })
+            -- The function below will be called before any actual modifications from lspkind
+            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+            -- before = function (entry, vim_item)
+            --   ...
+            --   return vim_item
+            -- end
+        })
+    }
 })
 
 -- Set configuration for specific filetype.
