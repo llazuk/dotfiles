8bfa48ebbd92cfc57ef276791473aa8961535cfa
llazuk <mailujspam@gmail.com>
2023-02-12 14:08:31 +1100

    2023-02-12

--- lua/kss/ide/cmp.lua
+++ lua/kss/ide/cmp.lua
@@ -110,7 +110,7 @@ cmp.setup.cmdline(':', {
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['sumneko_lua'].setup {
+require('lspconfig')['lua_ls'].setup {
     capabilities = capabilities
 }
 require('lspconfig')['jedi_language_server'].setup {
--- lua/kss/ide/lsp.lua
+++ lua/kss/ide/lsp.lua
@@ -21,7 +21,7 @@ local lsp_flags = {
   -- This is the default in Nvim 0.7+
   debounce_text_changes = 150,
 }
-require('lspconfig')['sumneko_lua'].setup{
+require('lspconfig')['lua_ls'].setup{
     on_attach = on_attach,
     flags = lsp_flags,
 }
